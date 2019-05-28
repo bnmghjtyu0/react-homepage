05aea2e1d9bcb9650156cf550f8f927caa2085da
Richard-柏錩 <richard.liao@program.com.tw>
2019-05-28 15:54:12 +0800

    axios-jsonp

--- src/App.js
+++ src/App.js
@@ -1,51 +1,29 @@
-import React, { useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import axios from "axios";
-import qs from "qs";
-import { Router, Route, Link } from "react-router-dom";
-import Material from "./Material";
+import React, { useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
+import jsonpAdapter from 'axios-jsonp'
+import qs from 'qs'
+import { Router, Route, Link } from 'react-router-dom'
+import Material from './Material'
 
 const App = () => {
   useEffect(() => {
-    axios
-      .get(`/auth/apilogin/?account=lora_jack&password=lora_170602`)
-      .then(res => {
-        const sid = res.data[0].retVal;
-        localStorage.setItem("sid", sid);
-      });
-    const sid = "d87760ce79558d469abfa350f5b680c6f09b999d";
-    const uid = "lora_jack";
-    const nid = "19572b16df36a50441386459949e303d3bbffbfc";
-    if (
-      window.location.hostname === "localhost" ||
-      window.location.hostname === "127.0.0.1"
-    ) {
-      document.cookie = `SID=${sid}&UID=${uid}&NID=${nid}`;
-    } else {
-      document.cookie = `SID=${sid}&UID=${uid}&NID=${nid};domain=.${
-        window.location.hostname
-      };path=/`;
-    }
-  });
-  useEffect(() => {
-    const data = [{ account: "owen", password: "owen", langid: "tw" }];
-    const method = "managerlogin";
+    // 有 jsonp
     axios({
-      method: "post",
-      url: `/backend/${method}`,
-      headers: {
-        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-      },
-
-      data: qs.stringify({
-        sid: localStorage.getItem("sid"),
-        data: JSON.stringify(data)
-      })
+      url: 'https://zh.wikipedia.org/w/api.php?action=opensearch&format=json&search=rainbow',
+      adapter: jsonpAdapter,
     }).then(res => {
-      console.log(res);
-    });
-  });
+      console.log(res)
+    })
+
+    // 沒有 jsonp
+    // axios({
+    //   url: 'https://zh.wikipedia.org/w/api.php?action=opensearch&format=json&search=rainbow',
+    // }).then(res => {
+    //   console.log(res)
+    // })
+  })
   return (
     <div className="App">
       <header className="App-header">
@@ -59,7 +37,7 @@ const App = () => {
         <Route path="/material/" component={Material} />
       </header>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
